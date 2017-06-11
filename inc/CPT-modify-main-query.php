cd0ef8a6bfe58b2d82fe942571c3fe2c720db9e1
Marek Melichar <koprivackej@gmail.com>
2017-06-11 21:56:11 +0200

    styles

--- inc/CPT-modify-main-query.php
+++ inc/CPT-modify-main-query.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * Modify Main WP Loop Query
  */
@@ -7,7 +7,7 @@ add_action( 'pre_get_posts', 'xroom_modify_main_query' );
 
 function xroom_modify_main_query( $query )
 {
-    // show movies on homepage
+    // show custom post types on homepage
     if ( $query->is_home() && $query->is_main_query() ) {
         $query->set( 'post_type', array( 'section', 'banner', 'footer' ) );
     }
