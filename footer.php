d5f5fecab4b47ee4ce9a5402a16bb01904ec27ad
Marek Melichar <koprivackej@gmail.com>
2017-06-25 19:17:38 +0200

    new design stage 25.6. template side done

--- footer.php
+++ footer.php
@@ -3,19 +3,18 @@
 
 	</main>
 
-		<footer class="footer blue-bg">
-			<div class="container">
-				<?php if ( get_theme_mod( 'footer_logo' ) ) : ?>
-						<div class='footer-logo'>
-								<a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><img src='<?php echo esc_url( get_theme_mod( 'footer_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'></a>
-						</div>
-				<?php endif ?>
-				<p>
-					<?php echo get_theme_mod('copy_by') ?>
-				</p>
-				<p>
-					<?php echo get_theme_mod('copy_text') ?>
-				</p>
+		<footer class="footer gray-bg">
+			<div class="row">
+				<div class="column size_50 text-right">
+					<?php if ( get_theme_mod( 'footer_logo' ) ) : ?>
+							<div class='footer-logo'>
+									<a href='<?php echo esc_url( home_url( '/' ) ); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><img src='<?php echo esc_url( get_theme_mod( 'footer_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'></a>
+							</div>
+					<?php endif ?>
+				</div>
+				<div class="column size_50 footer-copyright text-left">
+					<?php echo get_theme_mod('copy_by') ?> - <?php echo get_theme_mod('copy_text') ?>
+				</div>
 			</div>
 		</footer>
 
