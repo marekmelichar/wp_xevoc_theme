2c352d500a50c1f809d5816c8fff501b9fed142c
Marek Melichar <koprivackej@gmail.com>
2017-06-26 22:01:45 +0200

    semi finals

--- page-order.php
+++ page-order.php
@@ -9,12 +9,17 @@ get_header() ?>
 <?php if ( have_posts() ) : ?>
 		<?php while ( have_posts() ) : the_post() ?>
 			<div id="contact">
+				<div class="row">
+					<h1><?php the_title() ?></h1>
+				</div>
 				<div class="row <?php echo get_post_field('row_class'); ?>">
-					<div class="column size_60">
+					<div class="column size_100 text-center">
 						<?php the_post_thumbnail( 'full', array( 'class' => 'contact-img' ) ) ?>
 						<?php the_content() ?>
 					</div>
-					<div class="column size_40">
+				</div>
+				<div class="row order-form">
+					<div class="column size_100">
 						<?php echo do_shortcode( get_post_meta( get_the_id(), 'contact_form', true ) ); ?>
 					</div>
 				</div>
